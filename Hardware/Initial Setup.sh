e221a540e6bd6fe9efb3e4937aa7f0920af37a74
Eamonn <90015575+Eamonn8r@users.noreply.github.com>
2022-07-25 22:09:13 +1000

    Added script for Pi initial setup

---
+++ Hardware/Initial Setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# To initially configure the Pi, an image of the Raspberry Pi OS or equivalent should be written to the storage.
+# This script will execute some initial configuration options.
+
+# Update package repository:
+sudo apt update
+
+# Update packages:
+sudo apt full-upgrade
+
+# More to come
