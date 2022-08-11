6b37fc3f0b09608bb36168cd53af2a5231323598
Eamonn <90015575+Eamonn8r@users.noreply.github.com>
2022-08-11 21:54:23 +1000

    Added battery example

--- Hardware/Main Program/device.py
+++ Hardware/Main Program/device.py
@@ -1,10 +1,18 @@
+from pijuice import PiJuice # Import the battery module
+
 class Device:
     def __init__(self):
+        # Example property here
         self.name = "Example"
+        # Allows code to access the battery information
+        self.battery = PiJuice(1, 0x14)
     
+    # Used to warm up sensors/start gps etc
     def init(self):
         print("Initialising hardware and sensors")
 
+    # Returns the provided information. Possibly could be
+    # improved to use the format as with the battery above
     def getTemperature(self):
         pass
 
--- Hardware/Main Program/main.py
+++ Hardware/Main Program/main.py
@@ -2,7 +2,14 @@ from device import Device
 
 def main():
     device = Device()
+    device.init()
+    
     print(device.name)
+    print(device.battery.status.GetChargeLevel()["data"])
+
+
+
+
 
 
 if __name__ == "__main__":
