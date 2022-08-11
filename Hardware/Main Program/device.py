2d2ad5d251930866cadb9c84c96eafde5037641e
Eamonn <90015575+Eamonn8r@users.noreply.github.com>
2022-08-11 20:14:46 +1000

    Added starter functions

--- Hardware/Main Program/device.py
+++ Hardware/Main Program/device.py
@@ -1,3 +1,15 @@
 class Device:
     def __init__(self):
         self.name = "Example"
+    
+    def init(self):
+        print("Initialising hardware and sensors")
+
+    def getTemperature(self):
+        pass
+
+    def getHumidity(self):
+        pass
+
+    def getLocation(self):
+        pass
