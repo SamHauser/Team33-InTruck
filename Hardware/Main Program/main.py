c582b4ed91bb7b62b7c440b3c30bd86bad888c56
Eamonn <90015575+Eamonn8r@users.noreply.github.com>
2022-09-07 21:55:35 +1000

    Added saving data locally

--- Hardware/Main Program/main.py
+++ Hardware/Main Program/main.py
@@ -2,10 +2,13 @@ from device import Device
 import time
 import json
 import socket
+import sqlite3
+from contextlib import closing
 import paho.mqtt.client as mqtt
 from decouple import config
 
 DEVICE_NAME = socket.gethostname()
+LOCAL_DB_NAME = "local_cache.db"
 # To use the config() functions below, create a file called .env in the main program folder.
 # Add the info like this, line by line:
     # MQTT_BROKER_PORT=1883
@@ -59,6 +62,12 @@ def main():
     device = Device()
     device.init()
 
+    # Configure local storage
+    with closing(sqlite3.connect(LOCAL_DB_NAME)) as connection:
+        with closing(connection.cursor()) as db_cursor:
+            db_cursor.execute("CREATE TABLE IF NOT EXISTS stored_messages (json_payload TEXT)")
+            connection.commit()
+
     mqttc = connect_mqtt(MQTT_ADDRESS, MQTT_PORT, DEVICE_NAME, MQTT_USERNAME, MQTT_PASS)
     # Give time for it to connect
     for _ in range(10):
@@ -77,40 +86,44 @@ def main():
         MessageElement("battery", 10, device.battery_info)
     ]
 
-    # Main loop for device
-    while True:
-        message_payload = {}
-        ref_time = time.monotonic()
-        for element in message_elements:
-            if ref_time - element.last_sent > element.send_freq:
-                message_payload[element.name] = element.data
-                element.last_sent = ref_time
-        
-        if len(message_payload) != 0:
-            # Add device name and timestamp
-            message_payload["device_name"] = DEVICE_NAME
-            message_payload["timestamp"] = time.time()
-            # Set publish success for each message
-            publish_success = False
-            # connected_flag should be true if connected. When disconnected will take ~1 min to change to false.
-            # Changes back to true pretty quick on reconnect. This is done with the callback functions.
-            if mqttc.connected_flag:
-                # Attempt to publish message above as JSON
-                result = mqttc.publish("python/mqtt", json.dumps(message_payload), qos=1)
-                try:
-                    # Give it a sec for the server to confirm it is published (for qos 1 and I think also 2)
-                    result.wait_for_publish(timeout=1)
-                    publish_success = result.is_published()
-                except RuntimeError as err_msg:
-                    print("Unable to publish:", err_msg)
-            # If it was unable to publish the message, save it locally for sending later (messages will include the original timestamp)
-            if not publish_success:
-                print("In the future will save to local storage here")
-            else:
-                print("Published message", message_payload)
+    with closing(sqlite3.connect(LOCAL_DB_NAME)) as connection:
+        with closing(connection.cursor()) as db_cursor:
+            # Main loop for device
+            while True:
+                message_payload = {}
+                ref_time = time.monotonic()
+                for element in message_elements:
+                    if ref_time - element.last_sent > element.send_freq:
+                        message_payload[element.name] = element.data
+                        element.last_sent = ref_time
+                
+                if len(message_payload) != 0:
+                    # Add device name and timestamp
+                    message_payload["device_name"] = DEVICE_NAME
+                    message_payload["timestamp"] = time.time()
+                    # Set publish success for each message
+                    publish_success = False
+                    # connected_flag should be true if connected. When disconnected will take ~1 min to change to false.
+                    # Changes back to true pretty quick on reconnect. This is done with the callback functions.
+                    if mqttc.connected_flag:
+                        # Attempt to publish message above as JSON
+                        result = mqttc.publish("python/mqtt", json.dumps(message_payload), qos=1)
+                        try:
+                            # Give it a sec for the server to confirm it is published (for qos 1 and I think also 2)
+                            result.wait_for_publish(timeout=1)
+                            publish_success = result.is_published()
+                        except RuntimeError as err_msg:
+                            print("Unable to publish:", err_msg)
+                    # If it was unable to publish the message, save it locally for sending later (messages will include the original timestamp)
+                    if not publish_success:
+                        db_cursor.execute("INSERT INTO stored_messages VALUES (?)", (json.dumps(message_payload),))
+                        connection.commit()
+                        print("Unable to publish, saved locally", message_payload)
+                    else:
+                        print("Published message", message_payload)
 
-        # Will change in future
-        time.sleep(1)
+                # Will change in future
+                time.sleep(1)
 
 
 if __name__ == "__main__":
